805a723954e56ec68ff6625e48852e2b2163b6ce
Mobin Skaria <mobin0@berkeley.edu>
2022-06-10 07:38:24 -0700

    Initial Commit

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    public class BankAccount
+    {
+        private double balance = 0;
+        public void Deposit(double depositted)
+        {
+            if (depositted > 0)
+            {
+                balance += depositted;
+            }
+            Console.WriteLine($"Deposited {depositted}");
+        }
+            public double GetBalance() {
+                return balance;
+            }
+
+
+        static void Main(string[] args)
+        {
+            BankAccount Bank = new BankAccount();
+            Console.WriteLine($"Initial Balance: {Bank.GetBalance()}");
+            Bank.Deposit(500);
+            Console.WriteLine($"New Balance: {Bank.GetBalance()}");
+
+
+        }
+    }
+}
